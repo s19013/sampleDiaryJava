e1dad74d51d381497f2f08c97a25a91f6ed0278a
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-04 16:04:26 +0900

    自動生成完了

---
+++ src/main/java/com/example/sampleDiaryJava/SampleDiaryJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleDiaryJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDiaryJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDiaryJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampleDiaryJava/SampleDiaryJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleDiaryJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDiaryJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
