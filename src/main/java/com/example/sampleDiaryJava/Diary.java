4336d7fc57b9e4c709860174d75034b5b9360578
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-04 21:07:18 +0900

    エンティティクラスを作った

---
+++ src/main/java/com/example/sampleDiaryJava/Diary.java
@@ -0,0 +1,29 @@
+package com.example.sampleDiaryJava;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//laravelで言うマイグレーションとかモデルみたいのを作ってると思う
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity //エンティティクラスであることを示すため
+public class Diary {
+    public Diary(String bodytext,LocalDateTime createDateTime){
+        this.bodytext = bodytext;
+        this.createDatetime = createDateTime;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 連番機能
+    private  Integer id;
+    @Column(nullable = false)
+    private  String bodytext;
+    //投稿日時
+    @Column(name = "create_datetime",nullable = false)
+    private LocalDateTime createDatetime;
+}
