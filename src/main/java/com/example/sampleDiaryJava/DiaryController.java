af2c9ff85ef4c3a86f14a8deca5f2c22d729537c
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-06 14:48:25 +0900

    コントローラーを作り日記を全件返すようにした

---
+++ src/main/java/com/example/sampleDiaryJava/DiaryController.java
@@ -0,0 +1,29 @@
+package com.example.sampleDiaryJava;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller //フロントにデータを渡す場合は@controller restapiをしたければ@RestController
+@RequestMapping("diary")
+public class DiaryController {
+    //Springでは、@Autowiredを付けるとプログラマがリポジトリクラスやサービスクラスをnewしなくても、
+    // オブジェクトを自動でセット（注入）してくれます。
+    @Autowired
+    DiaryRepository diaryRepository;
+
+    //日記一覧情報の取得
+    @GetMapping("summary")
+    public String summary(Model model){
+        //このIterableとは何だ?
+        //laravelでいうmodelクラスを使ったsql操作みたいなことをしてると思う
+        //つまり N + 1問題が起きるということだよね?
+        Iterable<Diary> diarys = diaryRepository.findAll();
+
+        //laravel でいう Viewにwithみたいなことしてる
+        model.addAttribute("diarys",diarys);
+        return "summary";
+    }
+}
